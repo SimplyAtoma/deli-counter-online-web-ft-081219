0921b72345909362ee35b5f7e20e0695d38deb4a
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-20 17:48:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(order)
+  index = 1
+  if order[0] == nil
+    puts "The line is currently empty."
+  else
+    order.each{ |name| 
+        puts " #{index}. #{name}"
+        index += 1}
+  end
+end 
